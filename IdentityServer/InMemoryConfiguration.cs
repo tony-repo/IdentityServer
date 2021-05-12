ddd3140f1aecd2d526ba392fc693cf8ba5bec2b8
anzhendongchn@163.com <anzhendongchn@163.com>
2021-05-12 10:24:31 +0800

    add identity server

---
+++ IdentityServer/InMemoryConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityServer
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("scope1"),
+                //new ApiScope("scope2"),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource("api1","#api1")
+                {
+                    //!!!重要
+                    Scopes = { "scope1"}
+                },
+                //new ApiResource("api2","#api2")
+                //{
+                //    //!!!重要
+                //    Scopes = { "scope2"}
+                //},
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedScopes = { "scope1" }
+                },
+            };
+    }
+}
